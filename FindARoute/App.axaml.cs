8197026ef20f98e0c3cc5d3aa863ccc1ff629962
AJ <aletha.e.jones@gmail.com>
2024-01-13 23:03:29 +0000

    Starting Data persistence
    & routing?
    https://habr.com/en/articles/462307/

--- FindARoute/App.axaml.cs
+++ FindARoute/App.axaml.cs
@@ -5,33 +5,26 @@ using Avalonia.Markup.Xaml;
 using FindARoute.ViewModels;
 using FindARoute.Views;
 
-namespace FindARoute
+namespace FindARoute;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public override void Initialize()
+    { AvaloniaXamlLoader.Load(this); }
+
+    public override void OnFrameworkInitializationCompleted()
     {
-        public override void Initialize()
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            AvaloniaXamlLoader.Load(this);
+            desktop.MainWindow = new MainWindow
+            { DataContext = new MainWindowViewModel() };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            { DataContext = new MainWindowViewModel() };
         }
 
-        public override void OnFrameworkInitializationCompleted()
-        {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel()
-                };
-            }
-            else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-            {
-                singleViewPlatform.MainView = new MainView
-                {
-                    DataContext = new MainWindowViewModel()
-                };
-            }
-
-            base.OnFrameworkInitializationCompleted();
-        }
+        base.OnFrameworkInitializationCompleted();
     }
 }
--- FindARoute/ViewModels/SettingsViewModel.cs
+++ FindARoute/ViewModels/SettingsViewModel.cs
@@ -3,17 +3,17 @@
 using Avalonia.Controls.Primitives;
 using FindARoute.Utilities;
 using ReactiveUI;
+using Splat;
+using System.Runtime.Serialization;
 
 namespace FindARoute.ViewModels
 {
-    public class SettingsViewModel : ReactiveObject
+    [DataContract]
+    public class SettingsViewModel : ReactiveObject, IRoutableViewModel
     {
-        public SettingsViewModel(MainWindowViewModel _MWVM)
-        {
-
-        }
-
+        #region Settings
         private string _WiFiStateStr = "Enabled";
+        [DataMember]
         public string WiFiStateStr
         {
             get => _WiFiStateStr;
@@ -21,13 +21,20 @@ namespace FindARoute.ViewModels
         }
 
         private bool _WiFiState = true;
-
+        [DataMember]
         public bool WiFiState
         {
             get => _WiFiState;
             set => this.RaiseAndSetIfChanged(ref _WiFiState, value);
         }
+        #endregion
 
+        public SettingsViewModel(MainWindowViewModel _MWVM, IScreen? _Screen = null)
+        {
+            HostScreen = _Screen ?? Locator.Current.GetService<IScreen>();
+        }
+
+        #region Command funcs
         public void Command_GoBack(object? Sender)
         {
             var MVM = Helpers.GetParentContext(Sender);
@@ -43,5 +50,10 @@ namespace FindARoute.ViewModels
             else
             { WiFiState = false; }
         }
+        #endregion
+
+        public string? UrlPathSegment => "/Settings";
+
+        public IScreen? HostScreen { get; }
     }
 }
